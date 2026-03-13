c91ca3d8fbf82babedc591e672d0dc0b5adea06d
Dathor07 <pastorrosdavid@gmail.com>
2026-03-13 14:24:47 +0100

    cambio en los atributis ofrecemos y solicitamos con copy.deepcopy

--- intercambio.py
+++ intercambio.py
@@ -1,11 +1,12 @@
+import copy
 from transacción import Transaccion
 
 class Intercambio(Transaccion):
     def __init__(self, usuario_origen, usuario_destino, ofrecemos, solicitamos):
         super().__init__(usuario_origen)
         self.usuario_destino = usuario_destino
-        self.ofrecemos = ofrecemos
-        self.solicitamos = solicitamos
+        self.ofrecemos = copy.deepcopy(list(ofrecemos))
+        self.solicitamos = copy.deepcopy(list(solicitamos))
 
     def ejecutar(self):
         if self.estado == 'Pendiente':
